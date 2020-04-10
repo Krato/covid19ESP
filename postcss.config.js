ed6578fc6a3a5d4a9b7f335beeadbce8c9a5f95a
Eric Lagarda <eric@infinety,es>
2020-04-10 14:44:36 +0200

    wip 3

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,17 @@
+const extensionsUsingCSS = ['vue', 'html'];
+
 module.exports = {
     plugins: {
         tailwindcss: {},
         'vue-cli-plugin-tailwind/purgecss': {
+        	content: [
+        		`./@(public|src)/**/*.@(${extensionsUsingCSS.join('|')})`,
+        		`./node_modules/vue-good-table/src/**/*.vue`
+    		],
         	whitelistPatterns: [
         		/^vgt-/,
+        		/vgt-$/,
+        		/^vgt-$/,
         		/^tooltip$/
         	],
         },
