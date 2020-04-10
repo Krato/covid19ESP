f56c3f515c6fbdfef5110e7b81cca5ac99c34344
Eric Lagarda <eric@infinety,es>
2020-04-10 14:51:08 +0200

    wip disable purge

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,19 @@
-const extensionsUsingCSS = ['vue', 'html'];
+// const extensionsUsingCSS = ['vue', 'html'];
 
 module.exports = {
     plugins: {
         tailwindcss: {},
-        'vue-cli-plugin-tailwind/purgecss': {
-        	content: [
-        		`./@(public|src)/**/*.@(${extensionsUsingCSS.join('|')})`,
-        		`./node_modules/vue-good-table/src/**/*.vue`
-    		],
-        	whitelistPatterns: [
-        		/^vgt-/,
-        		/vgt-$/,
-        		/^vgt-$/,
-        		/^tooltip$/
-        	],
-        },
+      //   'vue-cli-plugin-tailwind/purgecss': {
+      //   	content: [
+      //   		`./@(public|src)/**/*.@(${extensionsUsingCSS.join('|')})`,
+      //   		`./node_modules/vue-good-table/src/**/*.vue`
+    		// ],
+      //   	whitelistPatterns: [
+      //   		/^vgt-/,
+      //   		/vgt-$/,
+      //   		/^vgt-$/,
+      //   		/^tooltip$/
+      //   	],
+      //   },
     },
 };
