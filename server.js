f37051c4a77af6624b042b555a7d9f371d638ac7
Eric Lagarda <eric@infinety,es>
2020-04-10 15:47:05 +0200

    Fix routes heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
+const history = require('connect-history-api-fallback');
 
 let app = express();
 
