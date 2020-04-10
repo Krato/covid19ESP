06fdd1eadd6a8713afff826422b14175d7dc2f5c
Eric Lagarda <eric@infinety,es>
2020-04-10 15:37:25 +0200

    Fix pages history

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const path = require('path');
 const serveStatic = require('serve-static');
 
 let app = express();
+
+// Use a fallback for non-root routes (required for Vue router)
+//   NOTE: History fallback must be "used" before the static serving middleware!
+app.use(history());
+
 app.use(serveStatic(__dirname + "/dist"));
 
 const port = process.env.PORT || 5000;
