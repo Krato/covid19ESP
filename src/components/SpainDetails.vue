c327a4df39e0e725258ca28ea50418ec8556e6eb
Eric Lagarda <eric@infinety,es>
2020-04-10 13:46:29 +0200

    Fix

--- src/components/SpainDetails.vue
+++ src/components/SpainDetails.vue
@@ -346,11 +346,6 @@ export default {
 
         seriesByDay() {
 
-            // let caConfirmed = _.find(this.confirmed, {'CCAA': this.ca})
-            // let caRecovered = _.find(this.recovered, {'CCAA': this.ca})
-            // let caDeaths = _.find(this.deaths, {'CCAA': this.ca})
-            // let caCritical = _.find(this.critical, {'CCAA': this.ca})
-
             let caConfirmed = _.find(this.spainHistorical.confirmed, {'CCAA': this.ca})
             let caDeaths = _.find(this.spainHistorical.deaths, {'CCAA': this.ca})
             let caRecovered = _.find(this.spainHistorical.recovered, {'CCAA': this.ca})
