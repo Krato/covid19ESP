5120dab5df8cdc9fa735f05bb67dc2e96d844af9
Eric Lagarda <eric@infinety,es>
2020-04-10 14:01:06 +0200

    Fix server v2

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+
+let app = express();
+app.use(serveStatic(__dirname + "/dist"));
+
+const port = process.env.PORT || 5000;
+app.listen(port);
